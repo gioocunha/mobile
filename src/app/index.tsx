2c1684cba219107ebe19b423266f4cd44073c2cb
Giovanna Cunha <giovannacunhabr@gmail.com>
2024-12-12 17:13:03 -0300

    feature: página inicial e de login

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text } from "react-native"
+
+import { Welcome } from "@/components/welcome"
+import { Steps } from "@/components/steps"
+
 
 export default function Index() {
   return (
     <View
-    
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}    
-    >
-      <Text style={{fontSize: 22}}> Hello React </Text>
+        flex: 1, padding: 40, gap: 40}}>
+      <Welcome />
+      <Steps /> 
     </View>
-  ); 
+  )
 }
